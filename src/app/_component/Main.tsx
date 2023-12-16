749f2161b9b7b4054306918f4475fce4fa94bf71
김신영 <ksyq12@gmail.com>
2023-12-16 20:55:55 +0900

    main

--- src/app/_component/Main.tsx
+++ src/app/_component/Main.tsx
@@ -2,15 +2,26 @@ import Tab from './Tab';
 import TabProvider from './TabProvider';
 
 import styles from './Main.module.css';
+import Post from './Post';
 
 export default function Main() {
   return (
     <div className={styles.container}>
       <aside className={styles.left}>
-        <div className={styles.leftTempDiv}>
-        </div></aside>
-      <section></section>
-      <aside></aside>
+        <div className={styles.leftTempDiv}><h4>인기글</h4></div>
+      </aside>
+      <section>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </section>
     </div>
   );
 }
